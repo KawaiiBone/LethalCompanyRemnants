8c3e86ec7677bee4164a42d836da8fb0043187f7
MathiasRadder <mathias.radder@hotmail.com>
2024-02-25 20:34:48 +0100

    Updated the mod version

--- Remnants/Main.cs
+++ Remnants/Main.cs
@@ -32,7 +32,7 @@ namespace Remnants
         #region Variables
         private const string modGUID = "KawaiiBone.Remnants";
         private const string modName = "Remnants";
-        private const string modVersion = "1.0.0";
+        private const string modVersion = "1.1.0";
 
         public static Remnants Instance;
         private readonly Harmony _harmony = new Harmony(modGUID);
